0b76ba11bd28c8a52801e60df0a9dc5c914c4b27
nisjety <ima_stovers24@hotmail.com>
2023-12-06 12:50:16 +0100

    first commit

---
+++ src/main/java/MFactory/MFactory/MFactoryApplication.java
@@ -0,0 +1,13 @@
+package MFactory.MFactory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MFactoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MFactoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/MFactory/MFactory/MFactoryApplicationTests.java
@@ -0,0 +1,13 @@
+package MFactory.MFactory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MFactoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
